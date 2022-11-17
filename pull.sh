3f5acbdf76fba61972474909ca765e39b7a075b9
caleb <yocalebo@gmail.com>
2022-11-17 14:31:53 -0500

    try this again...

--- pull.sh
+++ pull.sh
@@ -2,13 +2,13 @@
 VERSION=74
 REVISION=2
 
-wget http://deb.debian.org/debian/pool/main/n/ndctl/ndctl_$VERSION.debian.tar.gz
-tar xf ndctl_$VERSION.debian.tar.gz
-rm ndctl_$VERSION.debian.tar.gz
-
 wget http://deb.debian.org/debian/pool/main/n/ndctl/ndctl_$VERSION.orig.tar.gz
 tar xf ndctl_$VERSION.orig.tar.gz --strip 1
 rm ndctl_$VERSION.orig.tar.gz
+
+wget http://deb.debian.org/debian/pool/main/n/ndctl/ndctl_$VERSION-$REVISION.debian.tar.xz
+tar xf ndctl_$VERSION-$REVISION.debian.tar.xz
+rm ndctl_$VERSION-$REVISION.debian.tar.xz
 
 cp msft.patch debian/patches
 echo 'msft.patch' >> debian/patches/series
